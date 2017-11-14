0aa9705b9d30471cf7b890973bb7b434df5f3c5f
Michael Farmer <mfarmer91@gmail.com>
2017-11-14 15:52:19 -0500

    added dice roll functionality to index.js

--- schema/main.js
+++ schema/main.js
@@ -1,15 +1,23 @@
 const {
     GraphQLSchema,
     GraphQLObjectType,
-    GraphQLString
+    GraphQLString,
+    GraphQLInt,
+    GraphQLList
 } = require('graphql');
+
+const roll = () => Math.floor(6 * Math.random()) + 1;
 
 const queryType = new GraphQLObjectType({
     name: 'RootQuery',
         fields: {
             hello: {
-            type: GraphQLString,
-            resolve: () => 'world'
+                type: GraphQLString,
+                resolve: () => 'world'
+            },
+            diceRoll: {
+                type: new GraphQLList (GraphQLInt),
+                resolve: () => [roll(), roll()]
             }
         }
 });
