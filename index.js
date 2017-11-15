f69754d2529f77f8665bc17de5790a11c62c0eca
Michael Farmer <mfarmer91@gmail.com>
2017-11-15 16:55:10 -0500

    connected dd app with http interface using express.js and express-graphql middleware

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const { graphql } = require('graphql');
 const readline = require('readline');
- 
+
 const mySchema = require('./schema/main');
+
+const graphqlHTTP = require('express-graphql');
+
+const express = require('express');
+
+const app = express();
+
+const rli = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 const { MongoClient } = require('mongodb');
 const assert = require('assert');
@@ -9,18 +20,17 @@ const assert = require('assert');
 const MONGO_URL = 'mongodb://localhost:27017/test';
 
 MongoClient.connect(MONGO_URL, (err, db) => {
-    assert.equal(null, err);
-    console.log('You connected to MongoDB server, non-fool.');
-})
- 
-const rli = readline.createInterface({
-   input: process.stdin,
-   output: process.stdout
- });
- 
-rli.question('Client Request: ', inputQuery => {
-   graphql(mySchema, inputQuery).then(result => {
-     console.log('Server Answer :', result.data);
-   });
-rli.close();
- });
+  assert.equal(null, err);
+  console.log('Connected to MongoDB server');
+
+  //defines route at /graphql and delegates handling of route to express-graphql middleware.  Now we can read the input query from the URL.
+  app.use('/graphql', graphqlHTTP({
+      schema: mySchema,
+      context: { db }
+  }));
+    
+    //this starts the app.
+    app.listen(3000, () => 
+        console.log('Running Express.js on port 3000, thug.')
+    );
+});
