576f8a20ac88c563a7e735988074bf08c5b8afcc
Michael Farmer <mfarmer91@gmail.com>
2017-11-27 18:36:40 -0500

    added relay containers

--- js/app.js
+++ js/app.js
@@ -1,27 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Quote from './quote'
+import Relay from 'react-relay';
+
+import Quote from './quote';
 
 class QuotesLibrary extends React.Component {
-    state = { allQuotes: [] };
-    
-    componentDidMount() {
-        this.state.allQuotes
-        fetch('/graphql?query={allQuotes {id, text, author}}').then(response => response.json()).then(json => this.setState(json.data)).catch(ex => console.error(ex))
-    }
+  state = { allQuotes: [] };
 
-    render() {
-        return (
-            <div className="quotes-list">
-                {this.state.allQuotes.map(quote =>
-                    <Quote key={quote.id} quote={quote} />
-                )}
-            </div>
-        )
-    }
+  componentDidMount() {
+    fetch(`/graphql?query={
+                            allQuotes {
+                              id,
+                              text,
+                              author
+                            }
+                          }`)
+      .then(response => response.json())
+      .then(json => this.setState(json.data))
+      .catch(ex => console.error(ex))
+  }
 
+  render() {
+    return (
+      <div className="quotes-list">
+        {this.state.allQuotes.map(quote =>
+          <Quote key={quote.id} quote={quote} />
+        )}
+      </div>
+    )
+  }
+}
+
+QuotesLibrary = Relay.createContainer(QuotesLibrary, {
+  fragments: {}
+});
+
+class AppRoute extends Relay.Route {
+  static routeName = 'App';
 }
 
 ReactDOM.render(
-    <QuotesLibrary />, document.getElementById('react')
+  <Relay.RootContainer
+    Component={QuotesLibrary}
+    route={new AppRoute()}
+  />,
+  document.getElementById('react')
 );
